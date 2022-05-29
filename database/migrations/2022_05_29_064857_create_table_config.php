5727288ac8f643a5ecaa17ce4f253b0702473be6
thierryviztech <thierry@viztech.com.sg>
2022-05-29 08:19:05 +0800

    create new config function

---
+++ database/migrations/2022_05_29_064857_create_table_config.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTableConfig extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('config', function (Blueprint $table) {
+            $table->id();
+            $table->string('Config_name');
+            $table->integer('Value');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('config');
+    }
+}
